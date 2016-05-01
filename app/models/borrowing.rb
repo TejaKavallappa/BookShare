72b6fadf8f3605d657a9ebd1122351e690e2858f
MerlinBlaise <tejasbk@gmail.com>
2016-05-01 01:33:41 -0700

    Fixed bug in borrowings

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -18,13 +18,13 @@ class Borrowing < ActiveRecord::Base
 
   belongs_to :book,
     class_name: "Book",
-    foreign_key: book_id,
-    primary_key: id
+    foreign_key: :book_id,
+    primary_key: :id
 
   belongs_to :owner,
     class_name: "User",
-    foreign_key: owner_id,
-    primary_key: id
+    foreign_key: :owner_id,
+    primary_key: :id
 
   belongs_to :borrower,
     class_name: "User",
