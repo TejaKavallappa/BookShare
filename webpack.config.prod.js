8dff382062ed370694c58883506b97ec870ac18d
MerlinBlaise <tejasbk@gmail.com>
2016-05-07 19:53:08 -0700

    Fix minified copy warning on heroku

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    entry: "./frontend/bookshare.jsx",
+    output: {
+        path: "./app/assets/javascripts",
+        filename: "bundle.js"
+    },
+    resolve: {
+                 extensions: ["", ".js", ".jsx"]
+             },
+    plugins:[
+      new webpack.DefinePlugin({
+        'process.env':{
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress:{
+          warnings: true
+        }
+      })
+    ],
+    module: {
+        loaders: [
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react']
+            }
+        }
+        ]
+    },
+    devtool: 'source-map'
+};
